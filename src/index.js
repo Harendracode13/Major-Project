c0558c42a14f963b4f811f59419c3bfde65a1275
Harendra Piprotar <piprotarharendra@gmail.com>
2024-11-05 21:50:36 +0530

    Database connection
    MongoDB database connected with server successfully

---
+++ src/index.js
@@ -0,0 +1,46 @@
+//require('dotenv').config({path:'./env'})
+import dotenv from "dotenv";
+//import mongoose from "mongoose";
+//import {DB_NAME} from "./constants.js";
+
+dotenv.config({
+	path:'./env'
+})
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+
+connectDB()
+.then(()=>{
+	app.listen(process.env.PORT || 8000,()=>{
+		console.log(`sever is ready and running on ${process.env.PORT}`);
+	})
+})
+.catch((err)=>{
+	console.log("mongodb cnnection failed");
+})
+
+
+/*
+
+/*
+import express from "express";
+const app=express();
+
+(async()=>{
+	try{
+		await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+		app.on("error :",(error)=>{
+			console.log("errr: ",error);
+			throw error;
+		})
+		app.listen(process.env.PORT,()=>{
+			console.log(`app is lisning on ${process.env.PORT}`);
+		})
+	}
+	catch (error){
+		console.log("ERROR: ",error)
+		throw error
+	}
+})
+()*/
