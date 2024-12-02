b278d7a2fbb59ad5d69855ed60b86312d6585237
Harendra Piprotar <piprotarharendra@gmail.com>
2024-12-02 23:03:10 +0530

    toggle for comment like

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -58,3 +58,43 @@ const toggleVideoLike = asyncHandler(async (req, res) => {
 
 }
 )
+
+const toggleCommentLike=asyncHandler(async(req,res)=>{
+
+    const { commentId }=req.params
+
+    if(!isValidObjectId(commentId)){
+        throw new ApiError(400,"invaid commentid")
+    }
+
+     const user=req.user._id
+    const likedcomment= await Like.findOne({
+
+            $and:[{comment:commentId},{likedBy:user}]
+        }
+    )
+
+    if(!likedcomment)
+    {
+        const likecomment=await Like.create(
+            {
+                comment:commentId,
+                likedBy:user
+            }
+        )
+        if(!likecomment)
+        {
+            throw new ApiError(500,"Detect error at server side")
+        }
+
+        return res.status(200)
+        .json(
+            new ApiResponse(
+            200,
+            likecomment,
+             "comment like is added"
+            )
+        )
+    }
+
+})
