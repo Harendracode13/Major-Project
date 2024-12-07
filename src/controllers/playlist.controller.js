cc487526d1b0d2059562bf743f7106d2c196688e
Harendra Piprotar <piprotarharendra@gmail.com>
2024-12-07 10:59:18 +0530

    create playlist

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -9,4 +9,30 @@ const createPlaylist = asyncHandler(async (req, res) => {
     const {name, description} = req.body
 
     //TODO: create playlist
+    if(!((name)&&(description)))
+    {
+        throw new ApiError(400,"name and description is requried")
+    }
+
+    const newPlaylist=await Playlist.create(
+        {
+        name,
+        description,
+        owner:req.user?._id
+        }
+    )
+
+    if(!newPlaylist)
+    {
+        throw new ApiError(500,"error while creating the playlist")
+    }
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            newPlaylist,
+            "Playlist created"
+        )
+    )
 })
