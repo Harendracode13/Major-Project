90418e38b9c5955315d978b6a70f8ea0313a096a
Harendra Piprotar <piprotarharendra@gmail.com>
2024-12-07 15:37:15 +0530

    endpoint for get playlists

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -36,3 +36,107 @@ const createPlaylist = asyncHandler(async (req, res) => {
         )
     )
 })
+
+const getUserPlaylists = asyncHandler(async (req, res) => {
+
+    const {userId} = req.params
+    //TODO: get user playlists
+    if(!isValidObjectId(userId))
+    {
+        throw new ApiError(400,"invaild userid")
+    }
+
+    const findplaylist=await Playlist.aggregate(
+        [
+            {
+                $match:{
+                     owner:new mongoose.Types.ObjectId(user._id)
+                }
+            },
+            {
+                $lookup:{
+                    from:"videos",
+                    localField:"videos",
+                    foreignField:"_id",
+                    as:"videos",
+                    pipeline:[
+                        {
+                            $lookup:{
+                                from:"users",
+                                localField:"owner",
+                                foreignField:"_id",
+                                as:"owner"
+                            }
+                        },
+                        {
+                            $addFields:{
+                                owner:{
+                                $first:"$owner"
+                                }
+                            }
+                        },
+                        {
+                            $project:{
+                                title:1,
+                                description:1,
+                                thumbnail:1,
+                                owner:{
+                                    username: 1,
+                                    fullName: 1,
+                                    avatar: 1,
+                                }
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                $lookup:{
+                    from:"users",
+                    localField:"owner",
+                    foreignField:"_id",
+                    as:"createdBy",
+                    pipeline:[
+                        {
+                            $project:{
+                                avatar:1,
+                                fullName:1,
+                                username:1
+                            }
+                        }
+                ]
+                }
+            },
+            {
+                    $addFields:{
+                        createdBy:{
+                            $first:"$createdBy"
+                        }
+                    }
+            },
+            {
+                $project:{
+                    videos  :1,
+                    createdBy:1,
+                    name:1,
+                    description:1
+                }
+            }
+        ]
+    )
+    if (!findplaylist) {
+        throw new ApiError(
+            500,
+            "No such playlist found"
+        )
+    }
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            findplaylist,
+            "Got playlist"
+        )
+    )
+})
